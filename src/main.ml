f1647b6aa2e1ed78c7eb025b6b9fafc0c32728cd
Christian Lindig <lindig@gmail.com>
2018-09-30 11:22:35 +0100

    Reformat using ocamlformat
    Signed-off-by: Christian Lindig <lindig@gmail.com>

--- src/main.ml
+++ src/main.ml
@@ -1,9 +1,6 @@
-
-
 module C = Cmdliner
 
-let hello name =
-  Printf.printf "Hello, %s!\n" name
+let hello name = Printf.printf "Hello, %s!\n" name
 
 module Command = struct
   let help =
@@ -11,31 +8,26 @@ module Command = struct
     ; `S "MORE HELP"
     ; `P "Use `$(mname) $(i,COMMAND) --help' for help on a single command."
     ; `S "BUGS"
-    ; `P "Check bug reports at https://github.com/lindig/hello/issues"
-    ]
+    ; `P "Check bug reports at https://github.com/lindig/hello/issues" ]
 
   let name' =
-    C.Arg.(value
-           & pos 0 string "world"
-           & info [] 
-             ~docv:"NAME" 
-             ~doc:"Name of person to greet; the default is 'world'." 
-          )
+    C.Arg.(
+      value & pos 0 string "world"
+      & info [] ~docv:"NAME"
+          ~doc:"Name of person to greet; the default is 'world'.")
 
   let hello =
     let doc = "Say hello to someone" in
-    C.Term.
-      ( const hello $ name'
-      , info "hello" ~doc ~man:help
-      )
+    C.Term.(const hello $ name', info "hello" ~doc ~man:help)
 end
 
 let main () =
-  try match C.Term.eval Command.hello ~catch:false with
-    | `Error _  -> exit 1
-    | _         -> exit 0
-  with exn -> 
-    Printf.eprintf "error: %s\n" (Printexc.to_string exn); 
+  try
+    match C.Term.eval Command.hello ~catch:false with
+    | `Error _ -> exit 1
+    | _ -> exit 0
+  with exn ->
+    Printf.eprintf "error: %s\n" (Printexc.to_string exn) ;
     exit 1
 
 let () = if !Sys.interactive then () else main ()
